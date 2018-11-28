85b9354ade492ea9486e893ea7846032e73a2d5f
Phil Hart <phil@Phils-iMac.local>
2018-11-28 15:01:56 -0700

    Stretch finished

--- index.js
+++ index.js
@@ -76,13 +76,73 @@ server.put("/api/cohorts/:id", (req, res) => {
       .catch(err => res.status(500).json({ error: err }));
   });
   
+// GET ALL STUDENTS
+
+server.get("/api/students", (req, res) => {
+    db("students")
+      .then(students => res.status(200).json(students))
+      .catch(err => res.status(500).json(err));
+  });
 
 
 
+// GET STUDENT BY ID
+
+server.get("/api/students/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  db("students")
+    .where({ id: id })
+    .then(name => res.status(200).json(name))
+    .catch(err => res.status(500).json({ error: err }));
+});
+
+//POST
+
+server.post("/api/students", (req, res) => {
+    const student = req.body;
+    console.log(req.body);
+  
+    db("students")
+      .insert(student)
+      .returning("id")
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(err => {
+        res.status(500).json({ message: "Error inserting", err });
+      });
+  });
+  
+
+  //UPDATE
+
+  server.put("/api/students/:id", (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
+    db("students")
+      .where({ id: id })
+      .update(changes)
+      .then(count => {
+        res.status(200).json({ count });
+      })
+      .catch(err => res.status(500).json({ error: err }));
+  });
 
 
+  //DELETE 
 
-
+  server.delete("/api/students/:id", (req, res) => {
+    const { id } = req.params;
+    db("students")
+      .where({ id: id })
+      .del()
+      .then(count => {
+        res.status(200).json({ count });
+      })
+      .catch(err => res.status(500).json({ error: err }));
+  });
+  
 
 
 
